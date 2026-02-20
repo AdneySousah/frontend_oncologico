6adfcd8b13f18401ff20a3cddbabf551b7befc29
adney <190656330+AdneySousah@users.noreply.github.com>
2026-02-20 16:11:00 -0300

    Ajuste endereco api para backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3002',
+    baseURL: 'http://72.60.253.54:3002',
 });
 
 api.interceptors.request.use((config) => {
