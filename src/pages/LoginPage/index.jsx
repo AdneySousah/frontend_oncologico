c346f0eb6839910ef0f79260ff0d4ccd34a2b822
adney <190656330+AdneySousah@users.noreply.github.com>
2026-02-25 14:43:23 -0300

    fix: update login page remove alert

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -19,7 +19,6 @@ export default function LoginPage() {
       await Login(email, password);
     } catch (error) {
       console.error("Erro ao logar", error);
-      alert("Falha no login. Verifique suas credenciais.");
     } finally {
       setLoading(false);
     }
